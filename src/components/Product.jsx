33971b64d47f12e27f24ae10384e18929be8715b
Zfikzri <zfikzri07@gmail.com>
2025-02-18 20:11:17 +0700

    fixing

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -18,7 +18,7 @@ function Product() {
             suits, educational tools, gift certificates and more.
           </p>
         </div>
-        <div className="flex p-30 gap-5">
+        <div className="flex p-20 gap-5">
             <div className="flex flex-col items-center justify-center text-center gap-4">
                 <img src={product1} alt="" />
                 <h1 className="text-black font-bold tracking-wide w-full text-3xl">HEATHER <br /> HONEY</h1>
@@ -28,14 +28,14 @@ function Product() {
             </div>
             <div className="flex flex-col items-center justify-center text-center gap-4">
                 <img src={product2} alt="" />
-                <h1 className="text-black font-bold tracking-wide w-full text-3xl">HEATHER <br /> HONEY</h1>
+                <h1 className="text-black font-bold tracking-wide w-full text-3xl">JARRAH <br /> HONEY</h1>
                 <h2 className='text-orange-500 text-2xl font-semibold'>$10.00</h2>
                 <p className='text-gray-500 max-w-md'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</p>
                 <button className='text-black flex gap-2 uppercase font-bold tracking-wider'><ShoppingBag className='w-5 h-5'/> Add To Cart</button>
             </div>
             <div className="flex flex-col items-center justify-center text-center gap-4">
                 <img src={product3} alt="" />
-                <h1 className="text-black font-bold tracking-wide w-full text-3xl">HEATHER <br /> HONEY</h1>
+                <h1 className="text-black font-bold tracking-wide w-full text-3xl">LINDEN <br /> HONEY</h1>
                 <h2 className='text-orange-500 text-2xl font-semibold'>$10.00</h2>
                 <p className='text-gray-500 max-w-md'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</p>
                 <button className='text-black flex gap-2 uppercase font-bold tracking-wider'><ShoppingBag className='w-5 h-5'/> Add To Cart</button>
--- src/components/Testimoni.jsx
+++ src/components/Testimoni.jsx
@@ -1,12 +1,20 @@
-import React from 'react'
+import avatar from '../assets/images/testimoni-1.png'
+import honey from '../assets/images/honey-2.png'
+import bee from '../assets/images/bee-1.png'
 
 function Testimoni() {
   return (
-    <div className='flex'>
-        <img src="" alt="" />
-        <div>
-            
+    <div className='relative'>
+        <img src={honey} alt="" className='absolute w-50 right-10 top-35 '/>
+        <img src={bee} alt="" className='absolute w-10 left-130 top-0 '/>
+        <div className='flex mt-30 font-primary gap-20 justify-center items-center'>
+        <img src={avatar} alt=""/>
+        <div className='flex flex-col gap-1 '>
+            <h3 className='text-gray-500 text-2xl italic max-w-3xl text-wrap'>"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi."</h3>
+            <h1 className='text-black font-bold text-2xl tracking-wide'>JULIA <br /> KLEIN</h1>
+            <h2 className='text-orange-500 uppercase font-bold tracking-wider'>Biologist</h2>
         </div>
+    </div>
     </div>
   )
 }
